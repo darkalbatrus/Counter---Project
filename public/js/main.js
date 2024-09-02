2f4aa440625aa1495f57add58a5e8b65d1459a0c
darkalbatrus <147101531+darkalbatrus@users.noreply.github.com>
2024-09-02 22:28:56 +0330

    add basic command to js

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+//!-- Elements --//
+const counterText = document.querySelector(".counter-text");
+const counterButtons = document.querySelectorAll(".btn");
+
+//!-- Initial Action --//
+let count = 0;
+counterText.textContent = count;
