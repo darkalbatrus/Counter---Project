7184ff19f37672fad2073ba1a9e0a179d331d212
darkalbatrus <147101531+darkalbatrus@users.noreply.github.com>
2024-09-02 22:29:42 +0330

    add forEach loop to js

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,11 @@ const counterButtons = document.querySelectorAll(".btn");
 //!-- Initial Action --//
 let count = 0;
 counterText.textContent = count;
+
+//!-- ForEach Loop --//
+counterButtons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const currentButton = event.target.dataset.btn;
+    counterFunc(currentButton);
+  });
+});
